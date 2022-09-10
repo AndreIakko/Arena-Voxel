b777c8e3793bd8750f85ea42793b0ffd4e413e2c
Iakkoise <63969450+AndreIakko@users.noreply.github.com>
2022-09-10 16:38:07 +0200

    Add files via upload

---
+++ iak_arena_voxel/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "cerulean"
+
+game "gta5"
+
+author 'Iakkoise'
+
+description "ArenaVoxel"
+
+discord "https://discord.gg/tqk3kAEr4f"
+
+this_is_a_map 'yes'
+
+--cord: -3740.52, -3012.20, 545.27
